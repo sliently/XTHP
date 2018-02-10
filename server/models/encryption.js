47e6e77a5b57878ba568779d58afb7a23a8a4a45
HaipingXiaotong <847186328@qq.com>
2018-02-10 15:19:52 +0800

    修改

--- server/models/encryption.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto')
-module.exports = {
-    encryption: (password) => {
-        const hash = crypto.createHash('sha1');
-        hash.update(password);
-        return hash.digest('hex')
-    }
-}
